f71c285d5dd777867808282ffcb63351a9e73c1a
Theprotocolwhisperer <chrismata10@gmail.com>
2024-02-22 04:21:09 -0300

    (feature) : Pallet indexing events

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ use std::str::FromStr;
 use cosmrs::rpc::{Client, SubscriptionClient, WebSocketClient};
 use cosmrs::tx::Tx; // We don't need this yet
 use futures::StreamExt;
-
+use std::fs;
 use tendermint_rpc::query::Query;
+//Dependencies for writing the file :
+use std::fs::OpenOptions;
+use std::io::Write;
+
+use crate::parser::types::{EventData, PalletListing};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Connect to the WebSocket endpoint of the Cosmos node
@@ -17,15 +22,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(driver.run());
 
     // Query for new blocks
-    let query = "tm.event = 'NewBlock'";
+    let query = "tm.event='Tx' AND wasm EXISTS";
     let mut subs = client_ws.subscribe(Query::from_str(query).unwrap()).await?;
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("newquery2.txt")
+        .expect("Failed to open or create file");
 
     while let Some(msg) = subs.next().await {
         match msg {
             Ok(event) => {
-                println!("Received new block: {:?}", event.data);
-                // Here you can add additional processing for the block data
-                println!("Finish processing the block :)");
+                println!("Received new block: {:?}", event.events);
+                // Attempt to serialize event.events to a JSON string
+                if let Ok(event_json_str) = serde_json::to_string(&event.events) {
+                    // Now attempt to deserialize event_json_str into EventData
+                    match serde_json::from_str::<EventData>(&event_json_str) {
+                        Ok(event_data) => {
+                            // Successful deserialization, proceed with event_data
+                            // Let's do a check to see if it's from the pallet contract
+
+                            let listing = PalletListing {
+                                owner: event_data.message_sender[0].to_string(),
+                                nft_address: event_data.wasm_create_auction_nft_address[0]
+                                    .to_string(),
+                                token_id: event_data.wasm_create_auction_nft_token_id[0]
+                                    .to_string(),
+                                min_price: event_data.wasm_create_auction_min_price[0].to_string(),
+                                block_height: event_data.tx_height[0].to_string(),
+                                txhash: event_data.tx_hash[0].to_string(),
+                                listed: true,
+                            };
+
+                            println!(
+                                "The contract address is: {:?}",
+                                event_data.wasm_create_auction_contract_address
+                            );
+
+                            println!(
+                                "The address of the nft is: {:?}",
+                                event_data.wasm_create_auction_nft_address
+                            );
+                            println!(
+                                "The token is: {:?}",
+                                event_data.wasm_create_auction_nft_token_id
+                            );
+
+                            println!("The owner is: {:?}", event_data.message_sender);
+                        }
+
+                        Err(_e) => {
+                            // Failed to deserialize, event may not match expected structure
+                            println!("This is not a pallet event.");
+                        }
+                    }
+                } else {
+                    println!("Failed to serialize event.events to a JSON string");
+                }
+
+                // Additional processing...
             }
             Err(e) => println!("Error processing new block: {:?}", e),
         }
--- src/parser/types.rs
+++ src/parser/types.rs
@@ -1,6 +1,67 @@
+use serde::{Deserialize, Serialize};
 pub struct AuctionDetails {
     pub contract_address: String,
     pub min_price: String, // Assuming this could be simplified as a String for demonstration
     pub nft_address: String,
     pub nft_token_id: String,
+}
+
+pub struct PalletListing {
+    pub owner: String,
+    pub nft_address: String,
+    pub token_id: String,
+    pub min_price: String,
+    pub block_height: String,
+    pub txhash: String,
+    pub listed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EventData {
+    #[serde(rename = "coin_spent.amount")]
+    coin_spent_amount: Vec<String>,
+    #[serde(rename = "coin_spent.spender")]
+    coin_spent_spender: Vec<String>,
+    #[serde(rename = "execute._contract_address")]
+    execute_contract_address: Vec<String>,
+    #[serde(rename = "message.action")]
+    message_action: Vec<String>,
+    #[serde(rename = "message.module")]
+    message_module: Vec<String>,
+    #[serde(rename = "message.sender")]
+    pub message_sender: Vec<String>,
+    #[serde(rename = "tm.event")]
+    tm_event: Vec<String>,
+    #[serde(rename = "tx.acc_seq")]
+    tx_acc_seq: Vec<String>,
+    #[serde(rename = "tx.fee")]
+    tx_fee: Vec<String>,
+    #[serde(rename = "tx.fee_payer")]
+    tx_fee_payer: Vec<String>,
+    #[serde(rename = "tx.hash")]
+    pub tx_hash: Vec<String>,
+    #[serde(rename = "tx.height")]
+    pub tx_height: Vec<String>,
+    #[serde(rename = "tx.signature")]
+    tx_signature: Vec<String>,
+    #[serde(rename = "wasm-create_auction._contract_address")]
+    pub wasm_create_auction_contract_address: Vec<String>,
+    #[serde(rename = "wasm-create_auction.min_price")]
+    pub wasm_create_auction_min_price: Vec<String>,
+    #[serde(rename = "wasm-create_auction.nft_address")]
+    pub wasm_create_auction_nft_address: Vec<String>,
+    #[serde(rename = "wasm-create_auction.nft_token_id")]
+    pub wasm_create_auction_nft_token_id: Vec<String>,
+    #[serde(rename = "wasm._contract_address")]
+    pub wasm_contract_address: Vec<String>,
+    #[serde(rename = "wasm.action")]
+    wasm_action: Vec<String>,
+    #[serde(rename = "wasm.recipient")]
+    wasm_recipient: Vec<String>,
+    #[serde(rename = "wasm.sender")]
+    wasm_sender: Vec<String>,
+    #[serde(rename = "wasm.spender")]
+    wasm_spender: Vec<String>,
+    #[serde(rename = "wasm.token_id")]
+    wasm_token_id: Vec<String>,
 }
